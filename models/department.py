78db908e8c231924aff19576a53458fa2c83255e
Akash Kumar R <akashkumar210874@gmail.com>
2026-05-30 21:10:38 +0530

    feat: Change department name from string to citext to implement case insensitive unique and migrated

--- models/department.py
+++ models/department.py
@@ -1,15 +1,21 @@
-
-from sqlalchemy import String, Any
+from sqlalchemy import Any, String
+from sqlalchemy.dialects.postgresql import CITEXT
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from models.entity import Entity, datetime_to_iso
+
 from models.employee_x_department import Employee_X_Department
+from models.entity import Entity, datetime_to_iso
+
 
 class Department(Entity):
-    __tablename__= "departments"
+    __tablename__ = "departments"
 
-    name: Mapped[str] = mapped_column(String(20), nullable=False, unique=True)
+    name: Mapped[str] = mapped_column(CITEXT(20), nullable=False, unique=True)
 
-    employees: Mapped[list["Employee"]] = relationship("Employee", secondary=Employee_X_Department.__table__,  back_populates="departments")
+    employees: Mapped[list["Employee"]] = relationship(
+        "Employee",
+        secondary=Employee_X_Department.__table__,
+        back_populates="departments",
+    )
 
     def to_api_dict(self) -> dict[str, Any]:
         """JSON-friendly representation (ISO 8601 for timestamps)."""
@@ -18,10 +24,5 @@ class Department(Entity):
             "name": self.name,
             "created_at": datetime_to_iso(self.created_at),
             "updated_at": datetime_to_iso(self.updated_at),
-            "deleted_at": datetime_to_iso(self.deleted_at)
+            "deleted_at": datetime_to_iso(self.deleted_at),
         }
-
-
-
-
-
