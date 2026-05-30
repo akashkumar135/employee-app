9dc0fea812316ae6cc7aac72303dbc57d7065ece
Akash Kumar R <akashkumar210874@gmail.com>
2026-05-31 01:04:32 +0530

    feat: Add seperate token creation for access and refresh tokens

--- auth/utils.py
+++ auth/utils.py
@@ -1,13 +1,14 @@
-
+from datetime import datetime, timedelta, timezone
 
 import bcrypt
-from datetime import datetime, timezone, timedelta 
 from jose import JWTError, jwt
 
-from config import settings 
+from config import settings
+
 
 def hash_password(plain: str) -> str:
     return bcrypt.hashpw(plain.encode(), bcrypt.gensalt()).decode()
+
 
 def verify_password(plain: str, hashed: str) -> bool:
     return bcrypt.checkpw(plain.encode(), hashed.encode())
@@ -15,17 +16,30 @@ def verify_password(plain: str, hashed: str) -> bool:
 
 def create_access_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + timedelta(minutes=settings.jwt_expiry_minutes)
+    to_encode["type"] = "access_token"
+    expire = datetime.now(timezone.utc) + timedelta(
+        minutes=settings.jwt_access_token_expiry_minutes
+    )
     to_encode["exp"] = expire
 
     return jwt.encode(to_encode, settings.jwt_secret, algorithm=settings.jwt_algorithm)
 
 
-def decode_access_token(token: str) -> dict | None:
+def create_refresh_token(data: dict):
+    to_encode = data.copy()
+    to_encode["type"] = "refresh_token"
+    expire = datetime.now(timezone.utc) + timedelta(
+        minutes=settings.jwt_refresh_token_expire_minutes
+    )
+    to_encode["exp"] = expire
+
+    return jwt.encode(to_encode, settings.jwt_secret, algorithm=settings.jwt_algorithm)
+
+
+def decode_token(token: str) -> dict | None:
 
     try:
         return jwt.decode(token, settings.jwt_secret, algorithms=settings.jwt_algorithm)
-    
+
     except JWTError:
         return None
-    
