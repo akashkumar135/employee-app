b87290fed19bdecdbb6276fb0a468fa9894d1cb6
Akash Kumar R <akashkumar210874@gmail.com>
2026-06-03 22:24:21 +0530

    feat: Add example for chat interface

---
+++ client/src/pages/ChatExample.tsx
@@ -0,0 +1,47 @@
+import {
+  ChatHeader,
+  ChatInputBox,
+  ChatMessageBody,
+  ChatMessageBox,
+  ChatWrapper,
+} from "../components/chat/Chat";
+
+import Icon from "../assets/message-icon.svg";
+import MessageIcon from "../assets/send-icon.svg";
+
+const ChatExample = () => {
+  return (
+    <ChatWrapper>
+      <ChatHeader label="Help Desk" iconUrl={Icon} />
+      <ChatMessageBody>
+        <ChatMessageBox isSend={true}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, iusto
+          aliquid quibusdam dolorem nemo veritatis debitis molestias mollitia
+          sint ex distinctio quae quidem incidunt modi hic totam nostrum
+          voluptates architecto.
+        </ChatMessageBox>
+        <ChatMessageBox isSend={true}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, iusto
+          aliquid quibusdam dolorem nemo veritatis debitis molestias mollitia
+          sint ex distinctio quae quidem incidunt modi hic totam nostrum
+          voluptates architecto.
+        </ChatMessageBox>
+        <ChatMessageBox isSend={false}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, iusto
+          aliquid quibusdam dolorem nemo veritatis debitis molestias mollitia
+          sint ex distinctio quae quidem incidunt modi hic totam nostrum
+          voluptates architecto.
+        </ChatMessageBox>
+        <ChatMessageBox isSend={true}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, iusto
+          aliquid quibusdam dolorem nemo veritatis debitis molestias mollitia
+          sint ex distinctio quae quidem incidunt modi hic totam nostrum
+          voluptates architecto.
+        </ChatMessageBox>
+      </ChatMessageBody>
+      <ChatInputBox placeholder="Type your question" iconUrl={MessageIcon} />
+    </ChatWrapper>
+  );
+};
+
+export default ChatExample;
