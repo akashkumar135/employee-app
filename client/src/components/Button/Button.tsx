e12424b8f31dd1ab7b6c01f17869d8d89ea171ed
Akash Kumar R <akashkumar210874@gmail.com>
2026-06-04 23:02:14 +0530

    feat: Add onclick to button component

--- client/src/components/Button/Button.tsx
+++ client/src/components/Button/Button.tsx
@@ -4,15 +4,17 @@ type ButtonProps = {
   type?: "button" | "submit" | "reset";
   className?: string;
   children: React.ReactNode;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
 };
 
 const Button: React.FC<ButtonProps> = ({
   type = "button",
   className,
+  onClick,
   children,
 }) => {
   return (
-    <button type={type} className={className}>
+    <button type={type} className={className} onClick={onClick}>
       {children}
     </button>
   );
