f2f4ca0b7f1f8e183c2be857b6cab98a9821daa8
Akash Kumar R <akashkumar210874@gmail.com>
2026-06-08 09:39:02 +0530

    feat: Add path to sidenavbar items

--- client/src/components/navbar/SideNavbar/SideNavbar.tsx
+++ client/src/components/navbar/SideNavbar/SideNavbar.tsx
@@ -6,13 +6,11 @@ import EmployeeListIcon from "../../../assets/navbar-employee-icon.svg";
 const SideNavbar = () => {
   return (
     <aside className="navbar-container">
-      {/* <div className="navbar-item">
-        <div className="navbar-icon-container center">
-          <img src="./assets/navbar-employee-icon.svg" width="20" height="20" />
-        </div>
-        <span>Employee list</span>
-      </div> */}
-      <SideNavbarItem label="Employee list" iconUrl={EmployeeListIcon} />
+      <SideNavbarItem
+        url="/employee"
+        label="Employee list"
+        iconUrl={EmployeeListIcon}
+      />
     </aside>
   );
 };
--- client/src/components/navbar/SideNavbarItem/SideNavbarItem.tsx
+++ client/src/components/navbar/SideNavbarItem/SideNavbarItem.tsx
@@ -1,23 +1,27 @@
+import { Link } from "react-router";
+
 type SideNavbarItemProps = {
   label: string;
   iconUrl?: string;
   iconSize?: number;
+  url: string;
 };
 
 const SideNavbarItem: React.FC<SideNavbarItemProps> = ({
   label,
   iconUrl,
+  url,
   iconSize = 20,
 }) => {
   return (
-    <div className="navbar-item">
+    <Link to={url} className="navbar-item">
       {iconUrl && (
         <div className="navbar-icon-container center">
           <img src={iconUrl} width={iconSize} height={iconSize} />
         </div>
       )}
       <span>{label}</span>
-    </div>
+    </Link>
   );
 };
 
