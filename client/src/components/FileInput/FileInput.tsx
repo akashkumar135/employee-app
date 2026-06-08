58fc5b0895c379915a9441871d1c617324d0dd81
Akash Kumar R <akashkumar210874@gmail.com>
2026-06-08 15:52:37 +0530

    feat: Add file upload dialog component and its styles

--- client/src/components/FileInput/FileInput.tsx
+++ client/src/components/FileInput/FileInput.tsx
@@ -1,4 +1,6 @@
+import { MdClose } from "react-icons/md";
 import FileIcon from "../../assets/file-icon.svg";
+import Button from "../Button/Button";
 import "../Input/style.css";
 
 type FileInputProps = {
@@ -6,23 +8,51 @@ type FileInputProps = {
   label: string;
   name?: string;
   actionLabel: string;
+  fileName?: string;
+  onClick?: () => void;
+  onRemoveClick?: React.MouseEventHandler<HTMLButtonElement>;
 };
 
 const FileInput: React.FC<FileInputProps> = ({
   id,
   label,
   name,
+  fileName,
+  onRemoveClick,
   actionLabel,
+  onClick,
 }) => {
+  const handleRemoveClick: React.MouseEventHandler<HTMLButtonElement> = (
+    event,
+  ) => {
+    event.stopPropagation();
+
+    if (onRemoveClick) onRemoveClick(event);
+  };
+
   return (
-    <div className="input-wrapper">
+    <div className="input-wrapper" onClick={onClick}>
       <label htmlFor={id}>{label}</label>
       <div className="custom-file-upload-container">
         <label htmlFor={id} className="custom-file-upload">
-          <img src={FileIcon} width="16" height="16" />
-          {actionLabel}
+          {fileName && (
+            <div className="custom-file-value-wrapper">
+              <div className="custom-file-value">
+                <div>{fileName}</div>
+                <Button
+                  className="custom-file-remove center"
+                  onClick={handleRemoveClick}
+                >
+                  <MdClose size={16} />
+                </Button>
+              </div>
+            </div>
+          )}
+          <div className="custom-file-action-label">
+            <img src={FileIcon} width="16" height="16" />
+            {actionLabel}
+          </div>
         </label>
-        <input id={id} type="file" name={name} />
       </div>
     </div>
   );
---
+++ client/src/components/FileUpload/FileUpload.tsx
@@ -0,0 +1,73 @@
+import { MdClose } from "react-icons/md";
+import Button from "../Button/Button";
+import {
+  Dialog,
+  DialogBody,
+  DialogContent,
+  DialogFooter,
+  DialogTitle,
+} from "../Dialog/Dialog";
+
+import "./style.css";
+import { BsCloudArrowUpFill } from "react-icons/bs";
+import { FiUpload } from "react-icons/fi";
+
+type FileUploadDialogProps = {
+  onCancel: () => void;
+  onUpload: () => void;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
+  value: string;
+};
+const FileUploadDialog: React.FC<FileUploadDialogProps> = ({
+  onChange,
+  onCancel,
+  onUpload,
+  value,
+}) => {
+  return (
+    <Dialog>
+      <DialogBody className="custom-body">
+        <DialogTitle className="custom-title">
+          Upload Proof
+          <Button className="action-button icon-button" onClick={() => {}}>
+            <MdClose size={24} />
+          </Button>
+        </DialogTitle>
+        <DialogContent>
+          <label htmlFor="1234" className="file-upload">
+            <div className="file-upload-drag">
+              <BsCloudArrowUpFill size={64} color="#183072" opacity="0.1" />
+              <span>Drag & drop excel file here</span>
+            </div>
+            <span>Or</span>
+            <div className="file-second-option">
+              <FiUpload size={24} />
+              <p>Upload file</p>
+            </div>
+          </label>
+
+          <input id="1234" type="file" className="hidden" onChange={onChange} />
+        </DialogContent>
+        <DialogFooter>
+          <Button
+            type="button"
+            className="action-button cancel-button center"
+            onClick={onCancel}
+          >
+            Cancel
+          </Button>
+          <Button
+            type="button"
+            className="action-button confirm-button center"
+            onClick={onUpload}
+            disabled={!value}
+          >
+            Upload
+          </Button>
+        </DialogFooter>
+      </DialogBody>
+    </Dialog>
+  );
+};
+
+export default FileUploadDialog;
