1b977d98797f7b8686d50864745d51765afb9713
Akash Kumar R <akashkumar210874@gmail.com>
2026-06-11 14:28:43 +0530

    chore: Add alias for frontend imports

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,21 @@
 import react from "@vitejs/plugin-react";
+import path from "path";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@api-service": path.resolve(__dirname, "./src/api-service"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@router": path.resolve(__dirname, "./src/router"),
+      "@store": path.resolve(__dirname, "./src/store"),
+      "@types": path.resolve(__dirname, "./src/types"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+    },
+  },
 });
