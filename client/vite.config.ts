201195ffdcf2bbc576d38f3838216c3f463d5d84
Akash Kumar R <akashkumar210874@gmail.com>
2026-06-11 14:43:58 +0530

    fix: Resolve types inbuild issue

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
       "@pages": path.resolve(__dirname, "./src/pages"),
       "@router": path.resolve(__dirname, "./src/router"),
       "@store": path.resolve(__dirname, "./src/store"),
-      "@types": path.resolve(__dirname, "./src/types"),
+      "@type": path.resolve(__dirname, "./src/types"),
       "@utils": path.resolve(__dirname, "./src/utils"),
     },
   },
