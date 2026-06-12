81e901452c57200336c4929850a6909f107e511c
Akash Kumar R <akashkumar210874@gmail.com>
2026-06-12 09:13:19 +0530

    test: Add disable and onclick test to Button

--- client/src/tests/components/Button.test.tsx
+++ client/src/tests/components/Button.test.tsx
@@ -1,5 +1,6 @@
 import Button from "@components/Button/Button";
-import { render } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { describe, expect, it, vi } from "vitest";
 
 describe("Button component", () => {
@@ -11,5 +12,37 @@ describe("Button component", () => {
     );
 
     expect(container).toMatchSnapshot();
+  });
+
+  it("should disable the button", () => {
+    render(
+      <Button type="button" disabled={true} onClick={vi.fn()}>
+        Test
+      </Button>,
+    );
+
+    const buttonElement = screen.getByRole("button", { name: "Test" });
+
+    expect(buttonElement).toBeDisabled();
+  });
+
+  it("should trigger onClick event", async () => {
+    const mockOnClick = vi.fn();
+
+    render(
+      <Button type="button" onClick={mockOnClick}>
+        Test
+      </Button>,
+    );
+
+    const buttonElement = screen.getByRole("button", {
+      name: "Test",
+    });
+
+    await userEvent.type(buttonElement, "Hello");
+
+    await waitFor(() => {
+      expect(mockOnClick).toHaveBeenCalled();
+    });
   });
 });
